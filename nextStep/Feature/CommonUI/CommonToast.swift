1db91453175c7cdc0e93b0e47adee170e9765700
learnGrowD <dev_willd@naver.com>
2023-09-10 15:45:25 +0900

    toast 규격 설계

---
+++ nextStep/Feature/CommonUI/CommonToast.swift
@@ -0,0 +1,17 @@
+//
+//  CommonToast.swift
+//  nextStep
+//
+//  Created by 도학태 on 2023/09/10.
+//
+
+import UIKit
+/*
+ 3가지 타입을 정의할것이다.
+ 첫째는 아래에서 나오는 Toast
+ 둘쨰는 가운데
+ 셋째는 위에서 나오는 Toast
+ */
+class CommonToast: UIView {
+
+}
