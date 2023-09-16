65e909600c93b72fe1a6befac41e9ddf3bedb164
learnGrowD <dev_willd@naver.com>
2023-09-16 19:41:28 +0900

    commonRepository 추가

---
+++ nextStep/Base/Repository/CommonRepository.swift
@@ -0,0 +1,13 @@
+//
+//  CommonRepository.swift
+//  nextStep
+//
+//  Created by 도학태 on 2023/09/16.
+//
+
+import Foundation
+
+class CommonRepository {
+    static let shared = CommonRepository()
+    private init() {}
+}
