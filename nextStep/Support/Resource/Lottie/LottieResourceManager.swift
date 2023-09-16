85747efb6aa3311520745bb733149442e53bdf23
learnGrowD <dev_willd@naver.com>
2023-09-16 21:23:04 +0900

    R.swift file 경로 수정 및 resource 관리 규격 확정

---
+++ nextStep/Support/Resource/Lottie/LottieResourceManager.swift
@@ -0,0 +1,12 @@
+//
+//  LottieResourceManager.swift
+//  nextStep
+//
+//  Created by 도학태 on 2023/09/16.
+//
+
+import Foundation
+
+struct LottieResourceManager {
+    static let defaultLoadingName = "defaultLoading"
+}
--- nextStep/Support/Resource/R.generated.swift
+++ nextStep/Support/Resource/R.generated.swift
@@ -15,6 +15,8 @@ struct _R {
   var string: string { .init(bundle: bundle, preferredLanguages: nil, locale: nil) }
   var color: color { .init(bundle: bundle) }
   var info: info { .init(bundle: bundle) }
+  var font: font { .init(bundle: bundle) }
+  var file: file { .init(bundle: bundle) }
   var storyboard: storyboard { .init(bundle: bundle) }
 
   func string(bundle: Foundation.Bundle) -> string {
@@ -32,10 +34,17 @@ struct _R {
   func info(bundle: Foundation.Bundle) -> info {
     .init(bundle: bundle)
   }
+  func font(bundle: Foundation.Bundle) -> font {
+    .init(bundle: bundle)
+  }
+  func file(bundle: Foundation.Bundle) -> file {
+    .init(bundle: bundle)
+  }
   func storyboard(bundle: Foundation.Bundle) -> storyboard {
     .init(bundle: bundle)
   }
   func validate() throws {
+    try self.font.validate()
     try self.storyboard.validate()
   }
 
@@ -124,6 +133,37 @@ struct _R {
         }
       }
     }
+  }
+
+  /// This `_R.font` struct is generated, and contains static references to 2 fonts.
+  struct font: Sequence {
+    let bundle: Foundation.Bundle
+
+    /// Font `NotoSans-Regular`.
+    var notoSansRegular: RswiftResources.FontResource { .init(name: "NotoSans-Regular", bundle: bundle, filename: "NotoSans-Regular.ttf") }
+
+    /// Font `NotoSans-SemiBold`.
+    var notoSansSemiBold: RswiftResources.FontResource { .init(name: "NotoSans-SemiBold", bundle: bundle, filename: "NotoSans-SemiBold.ttf") }
+
+    func makeIterator() -> IndexingIterator<[RswiftResources.FontResource]> {
+      [notoSansRegular, notoSansSemiBold].makeIterator()
+    }
+    func validate() throws {
+      for font in self {
+        if !font.canBeLoaded() { throw RswiftResources.ValidationError("[R.swift] Font '\(font.name)' could not be loaded, is '\(font.filename)' added to the UIAppFonts array in this targets Info.plist?") }
+      }
+    }
+  }
+
+  /// This `_R.file` struct is generated, and contains static references to 2 resource files.
+  struct file {
+    let bundle: Foundation.Bundle
+
+    /// Resource file `NotoSans-Regular.ttf`.
+    var notoSansRegularTtf: RswiftResources.FileResource { .init(name: "NotoSans-Regular", pathExtension: "ttf", bundle: bundle, locale: LocaleReference.none) }
+
+    /// Resource file `NotoSans-SemiBold.ttf`.
+    var notoSansSemiBoldTtf: RswiftResources.FileResource { .init(name: "NotoSans-SemiBold", pathExtension: "ttf", bundle: bundle, locale: LocaleReference.none) }
   }
 
   /// This `_R.storyboard` struct is generated, and contains static references to 1 storyboards.
