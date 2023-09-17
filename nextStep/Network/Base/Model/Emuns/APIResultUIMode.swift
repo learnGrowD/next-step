a8ca04414371d1161a3c268654a7a18594689b7b
learnGrowD <dev_willd@naver.com>
2023-09-17 17:57:24 +0900

    APIRequest, APIResult 설계완료

--- nextStep/Network/Base/Model/Emuns/APIResultUIMode.swift
+++ nextStep/Network/Base/Model/Emuns/APIResultUIMode.swift
@@ -8,6 +8,13 @@
 import Foundation
 
 enum APIResultUIMode {
-    case warning
-    case login
+    case showWarning
+    case showToast
+    case showRetryView
+
+    /*
+     앱의 구조에 따라서 추가
+     ex) translateLogin: token이 만료되었거나 기타 등 login page로 전환한다.
+     */
+
 }
